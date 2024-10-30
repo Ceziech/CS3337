0b3a2618a984941b8951b4a046f50cf9cff8891f
mondisseous <asanch123@calstatela.edu>
2024-10-30 16:50:43 -0700

    updated the form in parking-details.html and added javascript

--- static/js/parking-details.js
+++ static/js/parking-details.js
@@ -17,4 +17,59 @@ document.addEventListener("DOMContentLoaded", function() {
             console.error("Cancel URL not found.")
         }
     });
+
+    const permitOptions = document.querySelectorAll(".permit-option");
+    const loginButton = document.querySelector(".login-button");
+
+    permitOptions.forEach(option => {
+        option.querySelector("p").style.display = "block";
+    })
+
+    permitOptions.forEach(option => {
+        const button = option.querySelector(".permit-button");
+        button.addEventListener("click", function () {
+            const isSelected = button.classList.contains("selected-button");
+
+            permitOptions.forEach(otherOption => {
+                otherOption.style.display = "block";
+                const otherButton = otherOption.querySelector(".permit-button");
+                otherButton.classList.remove("selected-button");
+                otherOption.querySelector("p").style.display = "block";
+                const checkmark = otherButton.querySelector(".checkmark");
+                if (checkmark) {
+                    checkmark.remove();
+                }
+            });
+
+            loginButton.style.display = "block";
+
+            if (!isSelected) {
+                permitOptions.forEach(otherOption => {
+                    if (otherOption !== option) {
+                        otherOption.style.display = "none";
+                    }
+                });
+                loginButton.style.display = "none";
+
+                button.classList.add("selected-button");
+                option.querySelector("p").style.display = "none";
+
+                if (!button.querySelector(".checkmark")) {
+                const checkmark = document.createElement("span");
+                checkmark.classList.add("checkmark");
+                checkmark.innerHTML = "&#10003;";
+                button.appendChild(checkmark);
+                }
+            }
+        });
+    });
+
+    loginButton.addEventListener("click", function () {
+        const loginUrl = loginButton.getAttribute("data-url");
+        if (loginUrl) {
+            window.location.href = loginUrl;
+        } else {
+            console.error("Login URL not found.");
+        }
+    })
 });
